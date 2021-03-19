6c68b0d2492ea0d7ef3ce9e58b1361f3ad8c162d
肇鑫（Zhao Xin） <owenzx@gmail.com>
2021-03-19 10:01:31 +0800

    * added sidebar_right image asset. * added SidebarViewController.swift.

---
+++ Xliff Tool/SidebarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarViewController.swift
+//  Xliff Tool
+//
+//  Created by zhaoxin on 2021/3/19.
+//  Copyright © 2021 zhaoxin. All rights reserved.
+//
+
+import Cocoa
+
+class SidebarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
