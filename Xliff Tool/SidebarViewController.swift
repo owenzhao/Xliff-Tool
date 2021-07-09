d67eb320e4cdf3748692c4de7563ee080c56b320
肇鑫（Zhao Xin） <owenzx@gmail.com>
2021-07-09 11:11:18 +0800

    * implemented Sidebar and SearchTextField.

--- Xliff Tool/SidebarViewController.swift
+++ Xliff Tool/SidebarViewController.swift
@@ -10,18 +10,31 @@ import Cocoa
 import RealmSwift
 
 class SidebarViewController: NSViewController {
+    static let userSelectItemDidChanged = Notification.Name("userSelectItemDidChanged")
+    private let defaults = UserDefaults.standard
+    
     @IBOutlet weak var searchField: NSSearchField!
     @IBOutlet weak var outlineView: NSOutlineView!
     
     private var expandedItem:RLMXTFile?
+    private var searchPredicate:NSPredicate? = nil {
+        didSet {
+            outlineView.reloadData()
+            
+            if searchPredicate != nil {
+                outlineView.expandItem(nil, expandChildren: true)
+            }
+        }
+    }
     
     private var notificationToken: NotificationToken? = nil
-    var files:Results<RLMXTFile>!
+    var files:Results<RLMXTFile>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(transUnitDidChanged(_:)), name: EditorViewController.transUnitDidChanged, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChangeNotification(_:)), name: NSControl.textDidChangeNotification, object: searchField)
         
         let url = (NSApp.delegate as? AppDelegate)?.databaseURL
         let realm = try! Realm(fileURL: url!)
@@ -55,9 +68,12 @@ class SidebarViewController: NSViewController {
     }
     
     @objc private func transUnitDidChanged(_ notification: Notification) {
-        let realm = files.first?.realm
+        guard let realm = files?.first?.realm else {
+            return
+        }
+        
         if let uid = notification.userInfo?["transUnit.uid"] as? String, uid != "nil",
-           let transUnit = realm?.object(ofType: RLMXTTransUnit.self, forPrimaryKey: uid) {
+           let transUnit = realm.object(ofType: RLMXTTransUnit.self, forPrimaryKey: uid) {
             
             let toExpandItem = transUnit.bodies.first?.files.first
             
@@ -72,30 +88,103 @@ class SidebarViewController: NSViewController {
             outlineView.scrollRowToVisible(row)
         }
     }
-}
-
-extension SidebarViewController:NSSearchFieldDelegate {
-    func searchFieldDidStartSearching(_ sender: NSSearchField) {
-        
+    
+    @IBOutlet weak var searchSourceCheckButton: NSButton! {
+        didSet {
+            searchSourceCheckButton.state = NSControl.StateValue(defaults.integer(forKey: UserDefaults.Key.searchSource.rawValue))
+        }
+    }
+    @IBOutlet weak var searchTargetCheckButton: NSButton! {
+        didSet {
+            searchTargetCheckButton.state = NSControl.StateValue(defaults.integer(forKey: UserDefaults.Key.searchTarget.rawValue))
+        }
+    }
+    @IBOutlet weak var searchNoteCheckButton: NSButton! {
+        didSet {
+            searchNoteCheckButton.state = NSControl.StateValue(defaults.integer(forKey: UserDefaults.Key.searchNote.rawValue))
+        }
+    }
+    @IBOutlet weak var searchOptionCaseSensitiveCheckButton: NSButton! {
+        didSet {
+            searchOptionCaseSensitiveCheckButton.state = NSControl.StateValue(defaults.integer(forKey: UserDefaults.Key.searchOptionCaseSensitive.rawValue))
+            searchOptionCaseSensitiveCheckButton.attributedAlternateTitle = NSAttributedString(string: "Aa", attributes: [.font : NSFont.boldSystemFont(ofSize: NSFont.systemFontSize)])
+        }
     }
     
-    func searchFieldDidEndSearching(_ sender: NSSearchField) {
-        
+    @IBAction func searchSourceCheckButtonClicked(_ sender: Any) {
+        let checkButton = sender as! NSButton
+        defaults.setValue(checkButton.state.rawValue, forKey: UserDefaults.Key.searchSource.rawValue)
+        search(text: searchField.stringValue)
+    }
+    
+    @IBAction func searchTargetCheckButtonClicked(_ sender: Any) {
+        let checkButton = sender as! NSButton
+        defaults.setValue(checkButton.state.rawValue, forKey: UserDefaults.Key.searchTarget.rawValue)
+        search(text: searchField.stringValue)
+    }
+    
+    @IBAction func searchNoteCheckButtonClicked(_ sender: Any) {
+        let checkButton = sender as! NSButton
+        defaults.setValue(checkButton.state.rawValue, forKey: UserDefaults.Key.searchNote.rawValue)
+        search(text: searchField.stringValue)
+    }
+    
+    @IBAction func searchOptionCaseSensitiveCheckButtonClicked(_ sender: Any) {
+        let checkButton = sender as! NSButton
+        defaults.setValue(checkButton.state.rawValue, forKey: UserDefaults.Key.searchOptionCaseSensitive.rawValue)
+        search(text: searchField.stringValue)
+    }
+    
+}
+
+extension SidebarViewController {
+    @objc func textDidChangeNotification(_ noti:Notification) {
+        if let tf = noti.userInfo?["NSFieldEditor"] as? NSTextView {
+            search(text: tf.string)
+        } else {
+            fatalError()
+        }
+    }
+    
+    private func search(text:String) {
+        if text.isEmpty {
+            searchPredicate = nil
+        } else {
+            // get options
+            let searchSource = (defaults.integer(forKey: UserDefaults.Key.searchSource.rawValue) == NSControl.StateValue.on.rawValue)
+            let searchTarget = (defaults.integer(forKey: UserDefaults.Key.searchTarget.rawValue) == NSControl.StateValue.on.rawValue)
+            let searchNote = (defaults.integer(forKey: UserDefaults.Key.searchNote.rawValue) == NSControl.StateValue.on.rawValue)
+            let searchOptionCaseSensitive = (defaults.integer(forKey: UserDefaults.Key.searchOptionCaseSensitive.rawValue) == NSControl.StateValue.on.rawValue)
+            
+            let searchOptionCaseSensitiveAction = searchOptionCaseSensitive ? "CONTAINS" : "CONTAINS[c]"
+            let searchSourcePart = searchSource ? String(format: "source \(searchOptionCaseSensitiveAction) '%@'", text) : nil
+            let searchTargetPart = searchTarget ? String(format: "target \(searchOptionCaseSensitiveAction) '%@'", text) : nil
+            let searchNotePart = searchNote ? String(format: "note \(searchOptionCaseSensitiveAction) '%@'", text) : nil
+            let searchStr = [searchSourcePart, searchTargetPart, searchNotePart].compactMap {$0}
+                .joined(separator: " OR ")
+            
+            searchPredicate = NSPredicate(format: searchStr, argumentArray: nil)
+        }
     }
 }
 
 extension SidebarViewController:NSOutlineViewDataSource, NSOutlineViewDelegate {
     func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
         if item == nil {
-            return files[index]
+            return files?[index] ?? 0
         }
         
-        let transUnits = (item as? RLMXTFile)?.body?.transUnits
+        let transUnits = (item as? RLMXTFile)?.body?.transUnits.filter(searchPredicate ?? NSPredicate(value: true))
         return transUnits![index]
     }
 
     func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
-        return item is RLMXTFile
+        if let file = item as? RLMXTFile,
+           let transUnits = file.body?.transUnits.filter(searchPredicate ?? NSPredicate(value: true)) {
+            return !transUnits.isEmpty
+        }
+        
+        return false
     }
 
     func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
@@ -104,10 +193,10 @@ extension SidebarViewController:NSOutlineViewDataSource, NSOutlineViewDelegate {
         }
         
         if item == nil {
-            return files.count
+            return files?.count ?? 0
         }
         
-        if let file = item as? RLMXTFile, let transUnits = file.body?.transUnits {
+        if let file = item as? RLMXTFile, let transUnits = file.body?.transUnits.filter(searchPredicate ?? NSPredicate(value: true)) {
             return transUnits.count
         }
         
